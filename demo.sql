22a28de4331480895ed4f67858147228e5de9a02
github-cloudlabsuser-224 <153939855+github-cloudlabsuser-224@users.noreply.github.com>
2024-04-26 17:47:46 +0000

    ex7 commit

---
+++ demo.sql
@@ -0,0 +1,7 @@
+
+-- Create a table for 5 products with product names and prices
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(50),
+    product_price DECIMAL(10, 2)
+);
